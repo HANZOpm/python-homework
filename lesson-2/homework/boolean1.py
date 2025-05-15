932f16cea9cba4baa2c411d2764b35ebd91d5ebb
Fazliddin Abduvoyitov <fazliddinabduvoiitov@gmail.com>
2025-05-15 13:50:45 +0500

    hello

--- lesson-2/homework/boolean1.py
+++ lesson-2/homework/boolean1.py
@@ -1,6 +1,63 @@
-username = input("Enter a username: ")
-password = input("Enter a password: ")
-if len(username) > 0 and len(password) > 0:
-    print(True)
-else:
-    print(False)
+# Task 1
+# username = input("Enter a username: ")
+# password = input("Enter a password: ")
+#
+# if " " in username or " " in password:
+#     print("Username and password can't contain a space.")
+# elif len(username) > 0 and len(password) > 0:
+#     print(True)
+# else:
+#     print("Please enter something.")
+# Task 2
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+#
+# if num1 == num2:
+#     print("They are equal")
+# else:
+#     print("They are not equal")
+# Task 3
+# num = float(input("Enter a number: "))
+# if num > 0 and num % 2 == 0:
+#     print(f"{num} is positive and even")
+# else:
+#     print(False)
+# Task 4
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+# num3 = float(input("Enter third number: "))
+#
+# if num1 != num2 and num2 != num3 and num3 != num1:
+#     print("Given numbers are all different")
+# else:
+#     print(False)
+# Task 5
+# string1 = input("Enter first string: ")
+# string2 = input("Enter second string: ")
+#
+# if len(string1) == len(string2):
+#     print("They have the same length")
+# else:
+#     print("They don't have the same length")
+# Task 6
+# num = int(input("Enter a number: "))
+#
+# if num % 3 == 0 and num % 5 == 0:
+#     print(f"{num} is divisible by both 3 and 5")
+# else:
+#     print(False)
+# Task 7
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+#
+# if num1 + num2 > 50:
+#     print(f"The sum of {num1} and {num2} is greater than 50")
+# else:
+#     print(f"The sum of {num1} and {num2} is not greater than 50")
+# Task 8
+# num = float(input("Enter a number: "))
+#
+# if 10 <= num <= 20:
+#     print(True)
+# else:
+#     print(False)
